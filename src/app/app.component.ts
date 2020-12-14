c42693b7ecb9191b03156d7e3f819958ac609c61
Mattlister <mdlister24@hotmail.com>
2020-12-14 18:34:04 +0000

    Added service with injectable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Final Fantasy Heroes';
@@ -15,19 +15,10 @@ constructor(private http: HttpClient) {
 }
 
 getPosts() {
-  this.posts = this.http.get('https://api.rawg.io/api/games?dates=2010-01-01,2020-12-31&developers=4132').subscribe(res => {
+  this.posts = this.http.get().subscribe(res => {
     this.posts = res;
     console.log(res);
   });
 }
 }
 
-@Component({
-  selector: 'app-root',
-  template: `
-    <h1>Tour of Heroes</h1>
-  `,
-  styles: ['h1 { font-weight: normal; }']
-})
-export class HeroAppComponent {
-}
