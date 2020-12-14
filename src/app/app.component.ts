01e35243962dbfd61d1f4bfa4b15b0f17ffde25d
Mattlister <mdlister24@hotmail.com>
2020-12-14 17:46:22 +0000

    Added fetching data setup and added service file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,5 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Final Fantasy Heroes';
+
+posts: any = '';
+
+constructor(private http: HttpClient) {
 }
 
+getPosts() {
+  this.posts = this.http.get('https://api.rawg.io/api/games?dates=2010-01-01,2020-12-31&developers=4132').subscribe(res => {
+    this.posts = res;
+    console.log(res);
+  });
+}
+}
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Tour of Heroes</h1>
+  `,
+  styles: ['h1 { font-weight: normal; }']
+})
+export class HeroAppComponent {
+}
